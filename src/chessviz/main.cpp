1e969ca06283fe75a91b79fbc58f091b11576e07
Vitaly-02 <arthas.menethil2002@bk.ru>
2021-04-28 15:11:25 +0700

    Make new_board.h and new_board.cpp

--- src/chessviz/main.cpp
+++ src/chessviz/main.cpp
@@ -1,44 +1,6 @@
 #include <iostream>
 #include <libchessviz/show_board.h>
-using namespace std;
-
-char** new_board()
-{
-    const int S = 8;
-    char** board;
-    board = new char* [S];
-    for (int i = 0; i < S; i++) 
-    {
-        board[i] = new char[S];
-    }
-    //add figures on board
-    board[0][0] = board[0][7] = 'r';
-    board[0][1] = board[0][6] = 'n';
-    board[0][2] = board[0][5] = 'b';
-    board[0][3] = 'q'; 
-    board[0][4] = 'k';
-    for (int i = 1, j = 0; j < S; j++) 
-    {
-        board[i][j] = 'p';
-    }
-    for (int i = 2; i < S - 2; i++) 
-    {
-        for (int j = 0; j < S; j++) 
-        {
-            board[i][j] = '_';
-        }
-    }
-    for (int i = 6, j = 0; j < S; j++) 
-    {
-        board[i][j] = 'P';
-    }
-    board[7][0] = board[7][7] = 'R';
-    board[7][1] = board[7][6] = 'N';
-    board[7][2] = board[7][5] = 'B';
-    board[7][3] = 'Q';
-    board[7][4] = 'K';
-    return board;
-}
+#include <libchessviz/new_board.h>
 
 int main() 
 {
