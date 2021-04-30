1342689905ce72dc113532c28737de1cc4748784
Vitaly-02 <arthas.menethil2002@bk.ru>
2021-04-30 20:49:35 +0700

    Make move_rule_for_queen.h and move_rule_for_queen.cpp

---
+++ src/libchessviz/move_rule_for_queen.cpp
@@ -0,0 +1,114 @@
+#include "move_rule_for_queen.h"
+
+bool move_rule_for_quenn(
+        char** board,
+    //    char figure,
+        int y_start,
+        int x_start,
+        int y_end,
+        int x_end)
+{
+    const int size = 8;
+    // (y - a, x - a)
+    if (y_start > y_end && x_start > x_end) {
+        if (y_start - y_end == x_start - x_end) {
+            // check another figure on the way
+            for (int i = y_start - 1, j = x_start - 1;
+                 i > y_end;
+                 --i, --j) {
+                if (board[i][j] != '_') {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+    // (y - a, x + a)
+    if (y_start > y_end && x_start < x_end) {
+        if (y_start - y_end == x_end - x_start) {
+            // check another figure on the way
+            for (int i = y_start - 1, j = x_start + 1;
+                 i > y_end;
+                 --i, ++j) {
+                if (board[i][j] != '_') {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+    // (y + a, x + a)
+    if (y_start < y_end && x_start < x_end) {
+        if (y_end - y_start == x_end - x_start) {
+            for (int i = y_start + 1, j = x_start + 1;
+                 i < y_end;
+                 ++i, ++j) {
+                if (board[i][j] != '_') {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+    // (y + a, x - a)
+    if (y_start < y_end && x_start > x_end) {
+        if (y_end - y_start == x_start - x_end) {
+            for (int i = y_start + 1, j = x_start - 1;
+                 i < y_end;
+                 ++i, --j) {
+                if (board[i][j] != '_') {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+    // horizontal moves
+    if (y_start == y_end
+        && (x_end >= 0 && x_end < size)) {
+        // check another figure on the way
+        // right move
+        if (x_start < x_end) {
+            for (int i = x_start + 1; i < x_end; ++i) {
+                if (board[y_start][i] != '_') {
+                    return false;
+                }
+            }
+        }
+        // left move
+        if (x_start > x_end) {
+            for (int i = x_start - 1; i > x_end; --i) {
+                if (board[y_start][i] != '_') {
+                    return false;
+                }
+            }
+        }
+        // all checks failed - true
+        return true;
+    }
+
+    // vertical moves
+    if (x_start == x_end
+        && (y_end >= 0 && y_end < size)) {
+        // check another figure on the way
+        // up move
+        if (y_start > y_end) {
+            for (int i = y_start - 1; i > y_end; --i) {
+                if (board[i][x_start] != '_') {
+                    return false;
+                }
+            }
+        }
+        // down move
+        if (y_start < y_end) {
+            for (int i = y_start + 1; i < y_end; ++i) {
+                if (board[i][x_start] != '_') {
+                    return false;
+                }
+            }
+        }
+        // all checks failed - true
+        return true;
+    }
+    return false;
+}
---
+++ src/libchessviz/move_rule_for_queen.h
@@ -0,0 +1,7 @@
+bool move_rule_for_quenn(
+        char** board,
+    //    char figure,
+        int y_start,
+        int x_start,
+        int y_end,
+        int x_end);
